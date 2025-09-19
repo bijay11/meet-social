a8656a58b55478d1a5e4141f2c008f81ca9ef845
Bijay Tamang <tmg.bijay11@gmail.com>
2025-09-19 06:32:35 -0400

    refactor endpoints

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
-from typing import Optional, List
-from fastapi import status, FastAPI, HTTPException, Response, Depends
+from fastapi import FastAPI
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from sqlalchemy.orm import Session
-from . import models, schemas, utils
-from .database import engine, get_db
+from . import models
+from .database import engine
+from .routers import post, user
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
 
 # Connecting to Database
 while True:
@@ -26,99 +23,5 @@ while True:
         print("Error:", error)
         time.sleep(2)
 
-@app.get("/posts", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts; """)
-    # posts = cursor.fetchall()
-
-    posts = db.query(models.Post).all()
-
-    return posts
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING *; """, 
-    #                (post.title, post.content, post.published))
-    # new_post = cursor.fetchone()
-
-    # # Save the result to DB
-    # conn.commit()
-
-    # Instead of passing title,content etc. use ** like this below.
-    new_post = models.Post(**post.model_dump())
-
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-
-    return new_post
-    
-@app.get("/posts/{id}", response_model=schemas.Post)
-def get_posts(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s; """, (str(id),))
-    # post = cursor.fetchone()
-
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    
-    return post
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM posts WHERE id =%s RETURNING * ; """,(str(id),))
-    # post = cursor.fetchone()
-
-    # conn.commit()
-
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    if post.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-    
-    post.delete(synchronize_session=False)
-    db.commit()
-    
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-@app.put("/posts/{id}", response_model=schemas.Post)
-def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, (str(id),)))
-    # post = cursor.fetchone()
-
-    # conn.commit()
-
-    update_post = db.query(models.Post).filter(models.Post.id == id)
-
-    if update_post.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-    
-    update_post.update(post.model_dump(), synchronize_session=False)
-
-    db.commit()
-    
-    return update_post.first()
-
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
-def create_user(user:schemas.UserCreate, db: Session = Depends(get_db)):
-     
-     # hash the password
-     hashed_password = utils.hash(user.password)
-     user.password = hashed_password
-
-     new_user = models.User(**user.model_dump())
-     db.add(new_user)
-     db.commit()
-     db.refresh(new_user)
-
-     return new_user
-
-@app.get("/users/{id}", response_model=schemas.UserResponse)
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} does not exist")
-    
-    return user
+app.include_router(post.router)
+app.include_router(user.router)
