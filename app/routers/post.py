40a58e9119e9152be6923448824d7450aa43c847
Bijay Tamang <tmg.bijay11@gmail.com>
2025-12-03 09:24:11 -0500

    implement limit and skip functionality

--- app/routers/post.py
+++ app/routers/post.py
@@ -6,18 +6,29 @@ from ..database import get_db
 
 router = APIRouter(prefix="/posts", tags=["posts"])
 
+
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+def get_posts(
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(oauth2.get_current_user),
+    limit: int = 10,
+    skip: int = 0,
+):
     # cursor.execute("""SELECT * FROM posts; """)
     # posts = cursor.fetchall()
 
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).limit(limit).offset(skip).all()
 
     return posts
 
+
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
-    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING *; """, 
+def create_post(
+    post: schemas.PostCreate,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(oauth2.get_current_user),
+):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING *; """,
     #                (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
 
@@ -26,28 +37,41 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db), current
 
     # Instead of passing title,content etc. use ** like this below.
 
-    new_post = models.Post(owner_id= current_user.id, **post.model_dump())
+    new_post = models.Post(owner_id=current_user.id, **post.model_dump())
 
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
 
     return new_post
-    
+
+
 @router.get("/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+def get_post(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(oauth2.get_current_user),
+):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s; """, (str(id),))
     # post = cursor.fetchone()
 
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} was not found",
+        )
+
     return post
 
+
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+def delete_post(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(oauth2.get_current_user),
+):
     # cursor.execute("""DELETE FROM posts WHERE id =%s RETURNING * ; """,(str(id),))
     # post = cursor.fetchone()
 
@@ -56,21 +80,32 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: models.Use
     post_query = db.query(models.Post).filter(models.Post.id == id)
 
     post = post_query.first()
-    
 
     if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-    
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} does not exist",
+        )
+
     if post.owner_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
-    
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized to perform requested action",
+        )
+
     post_query.delete(synchronize_session=False)
     db.commit()
-    
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
+
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+def update_post(
+    id: int,
+    post: schemas.PostCreate,
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(oauth2.get_current_user),
+):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, (str(id),)))
     # post = cursor.fetchone()
 
@@ -81,13 +116,19 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
     update_post = update_post_query.first()
 
     if update_post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-    
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} does not exist",
+        )
+
     if update_post.owner_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
-    
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not authorized to perform requested action",
+        )
+
     update_post_query.update(post.model_dump(), synchronize_session=False)
 
     db.commit()
-    
+
     return update_post
