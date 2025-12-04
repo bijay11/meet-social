84bb7350495c3828a3e3070cabd61d2a6efaed7d
Bijay Tamang <tmg.bijay11@gmail.com>
2025-12-04 09:17:14 -0500

    cleanup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 from . import models
 from .database import engine
 from .routers import post, user, auth
@@ -9,19 +6,6 @@ from .routers import post, user, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-# Connecting to Database
-while True:
-    try:
-        conn = psycopg2.connect(host='localhost', port=5433, database='meetsocial', user='postgres', password='learndb', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successful")
-        break
-    
-    except Exception as error:
-        print("Connecting to the database failed.")
-        print("Error:", error)
-        time.sleep(2)
 
 app.include_router(post.router)
 app.include_router(user.router)
